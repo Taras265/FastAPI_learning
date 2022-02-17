4eb08dd7a4e3f6b56b210e75da814ebc121a277e
Taras265 <70756830+Taras265@users.noreply.github.com>
2022-02-17 18:13:01 +0200

    Lesson 2

--- books.py
+++ books.py
@@ -3,6 +3,15 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+BOOKS = {
+    'book_1': {'title': 'Title 1', 'author': 'Author 1'},
+    'book_2': {'title': 'Title 2', 'author': 'Author 2'},
+    'book_3': {'title': 'Title 3', 'author': 'Author 3'},
+    'book_4': {'title': 'Title 4', 'author': 'Author 4'},
+    'book_5': {'title': 'Title 5', 'author': 'Author 5'},
+}
+
+
 @app.get("/")
-async def first_api():
-    return {"message": "Hello world!"}
+async def read_all_books():
+    return BOOKS
