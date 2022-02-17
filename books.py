bf2f2d2dfd4648c7221903e7fbe925de1a2d2b9c
Taras265 <70756830+Taras265@users.noreply.github.com>
2022-02-17 20:16:59 +0200

    Lesson 4

--- books.py
+++ books.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
@@ -12,16 +13,35 @@ BOOKS = {
 }
 
 
+class DirectionName(str, Enum):
+    north = "North"
+    south = "South"
+    east = "East"
+    west = "West"
+
+
 @app.get("/")
 async def read_all_books():
     return BOOKS
 
 
 @app.get("/books/mybook")
-async def read_book(book_id: int):
+async def read_book():
     return {"book_title": "My favourite book"}
 
 
 @app.get("/books/{book_id}")
 async def read_book(book_id: int):
     return {"book_title": book_id}
+
+
+@app.get("/direction/{direction_name}")
+async def get_direction(direction_name: DirectionName):
+    if direction_name == DirectionName.north:
+        return {"direction": direction_name, "sub": "Up"}
+    if direction_name == DirectionName.south:
+        return {"direction": direction_name, "sub": "Down"}
+    if direction_name == DirectionName.west:
+        return {"direction": direction_name, "sub": "Left"}
+    return {"direction": direction_name, "sub": "Right"}
+
