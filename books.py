53a6c202b9dea5170d85c4198cc71dfd6774d5c3
Taras265 <70756830+Taras265@users.noreply.github.com>
2022-02-17 20:29:03 +0200

    Lesson 6

--- books.py
+++ books.py
@@ -22,3 +22,14 @@ async def read_all_books():
 async def read_book(book_name: str):
     return BOOKS[book_name]
 
+
+@app.post("/")
+async def create_book(title, author):
+    current_id = 0
+    if len(BOOKS) > 0:
+        for book in BOOKS:
+            x = int(book.split('_')[-1])
+            if current_id < x:
+                current_id = x
+    BOOKS[f'book_{current_id+1}'] = {'title': title, 'author': author}
+    return BOOKS[f'book_{current_id+1}']
